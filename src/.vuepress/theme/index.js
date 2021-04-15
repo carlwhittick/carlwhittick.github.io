69ea0db6f815b1197bfca705c848b52993dd3959
Carl Whittick <carlw@pop-os.localdomain>
2021-04-15 23:40:22 +0100

    Downgrade vuepress

--- src/.vuepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-
-const fooTheme = {
-  name: 'vuepress-theme-carlwhittick',
-  layouts: {
-    Layout: path.resolve(__dirname, 'layouts/Layout.vue'),
-    // 404: path.resolve(__dirname, 'layouts/404.vue'),
-  },
-}
